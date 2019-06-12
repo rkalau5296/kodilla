5cafc1b54dd461433978df983ce24721fe4ca7b8
rkalau <rafal.kalata.itservice@gmail.com>
2019-06-12 16:17:00 +0200

    adding dummy tests to TestSuite

--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -1,5 +1,29 @@
 package com.kodilla.testing.collection;
 
-public class CollectionTestSuite {
+import org.junit.*;
 
+import java.util.ArrayList;
+
+public class CollectionTestSuite {
+    ArrayList<Integer> numbers;
+    @Before
+    public void before(){
+        numbers = new ArrayList<>();
+        for (int i=0; i<20; i++){
+            numbers.add(i);
+        }
+    }
+
+    @Test
+    public void testOddNumbersExterminatorEmptyList (){
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        oddNumbersExterminator.exterminate(numbers);
+
+    }
+    @Test
+    public void testOddNumbersExterminatorNormalList (){
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        oddNumbersExterminator.exterminate(numbers);
+
+    }
 }
