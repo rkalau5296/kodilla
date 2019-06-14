e85d5858c965f36bffc08cde21f893effa1dcd9d
rkalau <rafal.kalata.itservice@gmail.com>
2019-06-14 15:48:34 +0200

    adding two tests to TestSuite: empty list, normal list

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -4,17 +4,18 @@ import java.util.ArrayList;
 
 public class OddNumbersExterminator {
 
-    ArrayList<Integer> exterminate(ArrayList<Integer> numbers){
+    ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
         ArrayList<Integer> oddNumbers = new ArrayList<>();
-
-        for (int i=0; i<numbers.size(); i++)
-        {
-            if(numbers.get(i)%2==0)
-            {
-                oddNumbers.add(numbers.get(i));
+        if (numbers == null) {
+            System.out.println("the list is empty");
+        } else {
+            for (int i = 0; i < numbers.size(); i++) {
+                if (numbers.get(i) % 2 == 0) {
+                    oddNumbers.add(numbers.get(i));
+                }
             }
-        }
 
+        }
         return oddNumbers;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 
 public class CollectionTestSuite {
     ArrayList<Integer> numbers;
+
     @Before
     public void before(){
         numbers = new ArrayList<>();
@@ -13,17 +14,27 @@ public class CollectionTestSuite {
             numbers.add(i);
         }
     }
+    private void checkIfIsOddElements(OddNumbersExterminator oddNumbersExterminator ){
+        ArrayList<Integer> evenNumberList = oddNumbersExterminator.exterminate(numbers);
+        for (int i = 0; i<evenNumberList.size(); i++){
+            if (evenNumberList.get(i)%2!=0){
+                System.out.println("Error. The list contains odd elements");
+                return;
+            }
+        }
+        System.out.println("Everything is OK. There is no odd elements.");
+    }
+
 
     @Test
     public void testOddNumbersExterminatorEmptyList (){
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        numbers = null;
         oddNumbersExterminator.exterminate(numbers);
-
     }
     @Test
     public void testOddNumbersExterminatorNormalList (){
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-        oddNumbersExterminator.exterminate(numbers);
-
+        checkIfIsOddElements(oddNumbersExterminator);
     }
 }
