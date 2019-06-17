bbad69c7e7c0d5b073ced53744baf1946caadb1f
rkalau <rafal.kalata.itservice@gmail.com>
2019-06-17 11:21:41 +0200

    adding two tests to TestSuite: empty list, normal list

--- kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
+++ kodilla-testing/src/main/java/com/kodilla/testing/collection/OddNumbersExterminator.java
@@ -6,16 +6,12 @@ public class OddNumbersExterminator {
 
     ArrayList<Integer> exterminate(ArrayList<Integer> numbers) {
         ArrayList<Integer> oddNumbers = new ArrayList<>();
-        if (numbers == null) {
-            System.out.println("the list is empty");
-        } else {
+
             for (int i = 0; i < numbers.size(); i++) {
                 if (numbers.get(i) % 2 == 0) {
                     oddNumbers.add(numbers.get(i));
                 }
             }
-
-        }
         return oddNumbers;
     }
 }
--- kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/collection/CollectionTestSuite.java
@@ -13,28 +13,54 @@ public class CollectionTestSuite {
         for (int i=0; i<20; i++){
             numbers.add(i);
         }
+        System.out.println("Test Case: begin");
     }
-    private void checkIfIsOddElements(OddNumbersExterminator oddNumbersExterminator ){
-        ArrayList<Integer> evenNumberList = oddNumbersExterminator.exterminate(numbers);
-        for (int i = 0; i<evenNumberList.size(); i++){
-            if (evenNumberList.get(i)%2!=0){
-                System.out.println("Error. The list contains odd elements");
-                return;
-            }
-        }
-        System.out.println("Everything is OK. There is no odd elements.");
+
+    @After
+    public void after(){
+        System.out.println("Test Case: end");
     }
+//    private void checkIfIsOddElements(OddNumbersExterminator oddNumbersExterminator ){
+//        ArrayList<Integer> evenNumberList = oddNumbersExterminator.exterminate(numbers);
+//        for (int i = 0; i<evenNumberList.size(); i++){
+//            if (evenNumberList.get(i)%2!=0){
+//                System.out.println("Error. The list contains odd elements");
+//                return;
+//            }
+//        }
+//        System.out.println("Everything is OK. There is no odd elements.");
+//    }
 
 
     @Test
     public void testOddNumbersExterminatorEmptyList (){
+        //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-        numbers = null;
-        oddNumbersExterminator.exterminate(numbers);
+        ArrayList<Integer> numbers1 = new ArrayList<>();
+        oddNumbersExterminator.exterminate(numbers1);
+        //When
+        ArrayList<Integer> result = oddNumbersExterminator.exterminate(numbers1);
+        //Then
+        Assert.assertTrue(result.isEmpty());
     }
     @Test
-    public void testOddNumbersExterminatorNormalList (){
+    public void testOddNumbersExterminatorEvenList (){
+        //Given
         OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
-        checkIfIsOddElements(oddNumbersExterminator);
+        //When
+        int result = oddNumbersExterminator.exterminate(numbers).get(1);
+        System.out.println("testing " + result);
+        //Then
+        Assert.assertEquals(2,  result);
+    }
+    @Test
+    public void testOddNumbersExterminatorOddList (){
+        //Given
+        OddNumbersExterminator oddNumbersExterminator = new OddNumbersExterminator();
+        //When
+        int result = oddNumbersExterminator.exterminate(numbers).get(1);
+        System.out.println("testing " + result);
+        //Then
+        Assert.assertNotEquals(3,  result);
     }
 }
