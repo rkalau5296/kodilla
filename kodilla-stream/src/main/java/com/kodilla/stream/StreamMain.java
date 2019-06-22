6f4e4ff1d576f444ef400eeb6fbf5c58f4e8048c
rkalau5296 <rafal.kalata.itservice@gmail.com>
2019-06-22 20:06:04 +0200

    add setting gradle and others

---
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -0,0 +1,24 @@
+package com.kodilla.stream;
+
+import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.lambda.*;
+import com.kodilla.stream.reference.FunctionalCalculator;
+
+public class StreamMain {
+    public static void main(String[] args) {
+//        Processor processor = new Processor();
+//        ExecuteSaySomething executeSaySomething = new ExecuteSaySomething();
+//        processor.execute(executeSaySomething);
+//        processor.execute(() -> System.out.println("This is an example text."));
+
+//        ExpressionExecutor expressionExecutor = new ExpressionExecutor();
+//        expressionExecutor.executeExpression(15,6, (a,b)->a-b);
+//        expressionExecutor.executeExpression(15,6, (a,b)->a+b);
+//
+//        expressionExecutor.executeExpression(15,6, FunctionalCalculator::addAToB);
+
+        PoemBeautifier poemBeautifier = new PoemBeautifier();
+        //poemBeautifier.beautify("AAA", "aaa", (a,b)->a + );
+
+    }
+}
---
+++ kodilla-stream/src/main/java/com/kodilla/stream/beautifier/PoemBeautifier.java
@@ -0,0 +1,8 @@
+package com.kodilla.stream.beautifier;
+
+public class PoemBeautifier {
+    public void beautify(String a, String b, PoemDecorator poemDecorator){
+        String text = "This is a very beautiful text";
+
+    }
+}
