4828c693242f52ba1e4590be5ca6c5f2188acf0b
rkalau5296 <rafal.kalata.itservice@gmail.com>
2019-06-22 08:48:03 +0200

    6.5 start working

---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Forum/statistics/Calculate.java
@@ -0,0 +1,21 @@
+package com.kodilla.testing.Forum.statistics;
+
+import java.util.List;
+
+public class Calculate implements Statistics{
+
+    @Override
+    public List<String> usersNames() {
+        return null;
+    }
+
+    @Override
+    public int postsCount() {
+        return 0;
+    }
+
+    @Override
+    public int commentsCount() {
+        return 0;
+    }
+}
---
+++ kodilla-testing/src/main/java/com/kodilla/testing/Forum/statistics/Statistics.java
@@ -0,0 +1,9 @@
+package com.kodilla.testing.Forum.statistics;
+
+import java.util.List;
+
+public interface Statistics {
+    List<String> usersNames(); // list of users names
+    int postsCount();          // total quantity of forum posts
+    int commentsCount();       // total quantity of forum comments
+}
