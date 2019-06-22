c10bbdec8a446408bb4d07e54883aba7708f0e90
rkalau5296 <rafal.kalata.itservice@gmail.com>
2019-06-22 21:58:31 +0200

    7.2 Niemutowalny użytkownik forum

--- kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
+++ kodilla-stream/src/main/java/com/kodilla/stream/StreamMain.java
@@ -1,6 +1,7 @@
 package com.kodilla.stream;
 
 import com.kodilla.stream.beautifier.PoemBeautifier;
+import com.kodilla.stream.iterate.NumbersGenerator;
 
 
 public class StreamMain {
@@ -14,5 +15,7 @@ public class StreamMain {
         poemBeautifier.beautify("AAA", (a)->"<<<<<"+text+">>>>>");
         poemBeautifier.beautify("AAA", (a)->"****"+text+"****");
 
+        System.out.println("Using Stream to generate even numbers from 1 to 20");
+        NumbersGenerator.generateEven(20);
     }
 }
