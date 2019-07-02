b94b137d64a9bb50cc6f9083b7b606661d538f47
rkalata <rafal.kalata@itmagination.com>
2019-07-02 08:11:24 +0200

    adding testCompile group: 'org.mockito', name: 'mockito-core', version: '2.10.0' to build.gradle

--- kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
+++ kodilla-testing/src/test/java/com/kodilla/testing/weather/mock/WeatherForecastTestSuite.java
@@ -4,12 +4,10 @@ import com.kodilla.testing.weather.stub.Temperatures;
 import com.kodilla.testing.weather.stub.WeatherForecast;
 import org.junit.Assert;
 import org.junit.Test;
-
 import java.util.HashMap;
 
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
-
 
 public class WeatherForecastTestSuite {
     @Test
@@ -23,6 +21,7 @@ public class WeatherForecastTestSuite {
         temperaturesMap.put(3, 25.2);
         temperaturesMap.put(4, 26.1);
         when(temperaturesMock.getTemperatures()).thenReturn(temperaturesMap);
+
         WeatherForecast weatherForecast = new WeatherForecast(temperaturesMock);
 
         //When
