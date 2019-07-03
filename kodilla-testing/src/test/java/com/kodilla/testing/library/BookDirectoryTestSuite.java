1e969e807d29a6688d4bd15a3e46dd2ca2c1b925
rkalata <rafal.kalata@itmagination.com>
2019-07-03 15:58:22 +0200

    push

---
+++ kodilla-testing/src/test/java/com/kodilla/testing/library/BookDirectoryTestSuite.java
@@ -0,0 +1,58 @@
+package com.kodilla.testing.library;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+public class BookDirectoryTestSuite {
+    private List<Book> generateListOfNBooks(int booksQuantity) {
+        List<Book> resultList = new ArrayList<Book>();
+        for(int n = 1; n <= booksQuantity; n++){
+            Book theBook = new Book("Title " + n, "Author " + n, 1970 + n);
+            resultList.add(theBook);
+        }
+        return resultList;
+    }
+    @Test
+    public void testListBooksWithConditionsReturnList() {
+        // Given
+        LibraryDatabase libraryDatabaseMock = mock(LibraryDatabase.class);
+        BookLibrary bookLibrary = new BookLibrary(libraryDatabaseMock);
+        List<Book> resultListOf0Books  = new ArrayList<Book>();
+        List<Book> resultListOf15Books = generateListOfNBooks(15);
+        List<Book> resultListOf40Books = generateListOfNBooks(40);
+
+        when(libraryDatabaseMock.listBooksWithCondition(anyString()))
+                .thenReturn(resultListOf15Books);
+        when(libraryDatabaseMock.listBooksWithCondition("ZeroBooks"))
+                .thenReturn(resultListOf0Books);
+        when(libraryDatabaseMock.listBooksWithCondition("FortyBooks"))
+                .thenReturn(resultListOf40Books);
+
+        // When
+        List<Book> theListOfBooks0 = bookLibrary.listBooksWithCondition("ZeroBooks");
+        List<Book> theListOfBooks15 = bookLibrary.listBooksWithCondition("Any title");
+        List<Book> theListOfBooks40 = bookLibrary.listBooksWithCondition("FortyBooks");
+
+        /* Then */
+        Assert.assertEquals(0, theListOfBooks0.size());
+        Assert.assertEquals(15, theListOfBooks15.size());
+        Assert.assertEquals(0, theListOfBooks40.size());
+    }
+
+//    @Test
+//    public void testListBooksWithConditionMoreThan20() {
+//        Assert.assertTrue(false);
+//    }
+//
+//    @Test
+//    public void testListBooksWithConditionFragmentShorterThan3() {
+//        Assert.assertTrue(false);
+//    }
+}
