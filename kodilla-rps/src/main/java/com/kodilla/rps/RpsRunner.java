969eaf2b67fe71b98cd041cd28bac80ac26fe5d0
rkalata <rafal.kalata@itmagination.com>
2019-07-08 14:28:14 +0200

    7.7 prs still working

--- kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
+++ kodilla-rps/src/main/java/com/kodilla/rps/RpsRunner.java
@@ -8,7 +8,7 @@ public class RpsRunner {
         Scanner myObj = new Scanner(System.in);
         System.out.println("Podaj swoje imię");
         String userName = myObj.nextLine();
-        int userChoice;
+        char userChoice;
         int computerChoice;
         int userResult = 0;
         int computerResult = 0;
@@ -26,22 +26,43 @@ public class RpsRunner {
         while(roundQuantity>0){
             System.out.println("Runda " + actualRound);
             System.out.println("Wybierz klawisz od 1 do 3. W każdej chwili możesz przerwać naciskając klawisz 'x', lub zacząć od nowa naciskając klawisz 'n'.");
-            userChoice = myObj.nextInt();
+            userChoice = myObj.next().charAt(0);
+
+            if(userChoice == 'x')
+            {
+                System.out.println("Czy na pewno zakończyć grę? y/n");
+                userChoice = myObj.next().charAt(0);
+                if(userChoice=='y')
+                    return;
+                else if(userChoice=='n')
+                    continue;
+                else System.out.println("zły wybór");
+            }
+            if(userChoice == 'n')
+            {
+                System.out.println("Czy na pewno zakończyć aktualną grę? y/n");
+                userChoice = myObj.next().charAt(0);
+                if(userChoice=='y')
+                {
+
+                }
+                else if(userChoice=='n')
+                    continue;
+                else System.out.println("zły wybór");
+            }
             System.out.println("Wybrałeś: " + userChoice);
             Random random = new Random();
-            int x = 1;
-            int y = 3;
             computerChoice = random.nextInt(3)+1;
             System.out.println( "Komputer wybrał: " + computerChoice);
-            if((userChoice == 1 && computerChoice == 1)||(userChoice == 2 && computerChoice == 2)||(userChoice == 3 && computerChoice == 3))
+            if((userChoice == '1' && computerChoice == 1)||(userChoice == '2' && computerChoice == 2)||(userChoice == '3' && computerChoice == 3))
             {
                 System.out.println("Remis. Bez punktu dla każdego");
             }
-            if((userChoice == 1 && computerChoice==2)||(userChoice == 2 && computerChoice==3)||(userChoice == 3 && computerChoice== 1)){
+            if((userChoice == '1' && computerChoice==2)||(userChoice == '2' && computerChoice==3)||(userChoice == '3' && computerChoice== 1)){
                 System.out.println("Przegrałeś runde. Punkt dla komputera.");
                 computerResult = computerResult+1;
             }
-            if((computerChoice == 1 && userChoice==2)||(computerChoice == 2 && userChoice==3)||(computerChoice == 3 && userChoice== 1)){
+            if((computerChoice == '1' && userChoice==2)||(computerChoice == '2' && userChoice==3)||(computerChoice == '3' && userChoice== 1)){
                 System.out.println("Wygrałeś runde. Punkt dla Ciebie.");
                 userResult = userResult+1;
             }
