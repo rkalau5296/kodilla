9d176667fed37fb762d8f7be3ad813f59a2b3c80
rkalata <rafal.kalata@itmagination.com>
2019-08-28 09:20:56 +0200

    13.2 start working

---
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/challenges/ProductOrderService.java
@@ -0,0 +1,14 @@
+package com.kodilla.good.patterns.challenges;
+
+public class ProductOrderService {
+    private InformationService informationService;
+    private OrderService orderService;
+    private OrderRepository orderRepository;
+
+    public ProductOrderService(InformationService informationService, OrderService orderService, OrderRepository orderRepository) {
+        this.informationService = informationService;
+        this.orderService = orderService;
+        this.orderRepository = orderRepository;
+    }
+
+}
