8608dc08011bd896f6772bfcb38fed8e21c8c774
rkalata <rafal.kalata@itmagination.com>
2019-08-29 15:58:53 +0200

    refactor - move two classess to other location.

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/trial/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/trial/Main.java
@@ -1,4 +1,4 @@
-package com.kodilla.good.patterns.challenges;
+package com.kodilla.good.patterns.trial;
 
 public class Main {
     public static void main(String[] args) {
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/trial/MovieStore.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/trial/MovieStore.java
@@ -1,4 +1,4 @@
-package com.kodilla.good.patterns.challenges;
+package com.kodilla.good.patterns.trial;
 
 import java.util.ArrayList;
 import java.util.HashMap;
