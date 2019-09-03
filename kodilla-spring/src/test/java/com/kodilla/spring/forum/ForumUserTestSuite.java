d1959d41d42aec6bb41ed4349fed6281dbc1f7f7
rkalata <rafal.kalata@itmagination.com>
2019-09-03 15:35:17 +0200

    14.1 Keep working

--- kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
+++ kodilla-spring/src/test/java/com/kodilla/spring/forum/ForumUserTestSuite.java
@@ -1,14 +1,28 @@
 package com.kodilla.spring.forum;
 
+import com.kodilla.spring.shape.Circle;
+import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
 
-@RunWith(ForumUser.class)
+@RunWith(SpringRunner.class)
 @SpringBootTest
 public class ForumUserTestSuite {
     @Test
     public void testGetUsername() {
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
 
+        context.getBean(ForumUser.class);
+        ForumUser forumUser = new ForumUser();
+        //When
+        String name = forumUser.username;
+        //Then
+        Assert.assertEquals("John Smith", name);
     }
 }
