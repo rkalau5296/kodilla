bf319dfce5b4d3f043bc21b391f91b8b820bd311
rkalata <rafal.kalata.itservice@gmail.com>
2019-09-08 19:25:13 +0200

    13.5 keep working

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/FlightList.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/FlightList.java
@@ -2,6 +2,7 @@ package com.kodilla.good.patterns.Flights;
 
 import java.util.HashMap;
 import java.util.Map;
+import java.util.Objects;
 
 public class FlightList {
     private Map<String, Flight> flights = new HashMap<>();
@@ -20,5 +21,27 @@ public class FlightList {
     }
     public void addFlightToList (String connectingFlight, Flight flight) {
         flights.put(connectingFlight, flight);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FlightList that = (FlightList) o;
+        return Objects.equals(flights, that.flights) &&
+                Objects.equals(connectingFlight, that.connectingFlight);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(flights, connectingFlight);
+    }
+
+    @Override
+    public String toString() {
+        return "FlightList{" +
+                "flights=" + flights +
+                ", connectingFlight='" + connectingFlight + '\'' +
+                '}';
     }
 }
