81b620aa3b2e3f1b11696c51ff7d3c64f3e75b1d
rkalata <rafal.kalata.itservice@gmail.com>
2019-09-08 15:16:08 +0200

    13.5 keep working

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/Flight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/Flight.java
@@ -1,26 +1,40 @@
 package com.kodilla.good.patterns.Flights;
 
-import java.util.ArrayList;
 import java.util.HashMap;
 
 import java.util.Map;
 
 public class Flight {
-    public Map<String, String> getFlight() {
+    public Map<String, Map<String, String>> getFlight() {
 
-        Map<String, String> Flights = new HashMap<>();
-        Flights.put("Kraków", "Poznań");
-        Flights.put("Warszawa", "Poznań");
-        Flights.put("lódź", "Szczecin");
-        Flights.put("Poznań", "lódź");
-        Flights.put("Szczecin", "Katowice");
-        Flights.put("Olsztyn", "Opole");
-        Flights.put("Katowice", "Radom");
-        Flights.put("Radom", "Poznań");
-        Flights.put("Kielce", "Bydgoszcz");
-        Flights.put("Bydgoszcz", "Warszawa");
-        Flights.put("Opole", "Kielce");
+        Map<String, String> directFlightsKrakowPoznan = new HashMap<>();
+        directFlightsKrakowPoznan.put("Kraków", "Poznań");
 
-        return Flights;
+        Map<String, String> directFlightsLodzSzczecin = new HashMap<>();
+        directFlightsLodzSzczecin.put("lódź", "Szczecin");
+
+        Map<String, String> directFlightsPoznanLodz = new HashMap<>();
+        directFlightsPoznanLodz.put("Poznań", "lódź");
+
+        Map<String, String> directFlightsSzczecinKatowice = new HashMap<>();
+        directFlightsSzczecinKatowice.put("Szczecin", "Katowice");
+
+        Map<String, String> directFlightsOlsztynOpole = new HashMap<>();
+        directFlightsOlsztynOpole.put("Olsztyn", "Opole");
+
+        Map<String, String> directFlightsKatowiceOlsztyn = new HashMap<>();
+        directFlightsKatowiceOlsztyn.put("Katowice", "Radom");
+
+        Map<String, Map<String, String>> connectingFlights = new HashMap<>();
+        connectingFlights.put("Radom", directFlightsKrakowPoznan);
+        connectingFlights.put("Warszawa", directFlightsLodzSzczecin);
+        connectingFlights.put("Olsztyn", directFlightsPoznanLodz);
+        connectingFlights.put("Tarnów", directFlightsSzczecinKatowice);
+        connectingFlights.put("Nowy Sącz", directFlightsOlsztynOpole);
+        connectingFlights.put("Gdansk", directFlightsKatowiceOlsztyn);
+
+        return connectingFlights;
     }
+
+
 }
