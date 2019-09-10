b2dff454cf27eb400a9e58351baccfab04576b81
rkalata <rafal.kalata@itmagination.com>
2019-09-10 12:40:05 +0200

    13.5 still working

--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/Flight.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/Flight.java
@@ -30,7 +30,7 @@ public class Flight {
         List<Flight> flightslist = new ArrayList<>();
 
         flightslist.add(new Flight("Warszawa", "Kraków"));
-        flightslist.add(new Flight("Warszawa", "Gdańsk"));
+        flightslist.add(new Flight("Warszawa", "Opole"));
         flightslist.add(new Flight("Kraków", "Gdańsk"));
         flightslist.add(new Flight("Gdańsk", "Wrocław"));
         flightslist.add(new Flight("Gdańsk", "Katowice"));
@@ -43,10 +43,27 @@ public class Flight {
         return flightslist;
     }
     public static void findFlightsFrom (String city, List<Flight> flightsList) {
-        System.out.println(flightsList.stream().filter(s->s.getFlightFrom().equals(city)).collect(Collectors.toList()));
+        System.out.println(flightsList.stream()
+                .filter(s->s.getFlightFrom().equals(city))
+                .collect(Collectors.toList()));
     }
     public static void findFlightsTo (String city, List<Flight> flightsList) {
-        System.out.println(flightsList.stream().filter(s->s.getFlightTo().equals(city)).collect(Collectors.toList()));
+        System.out.println(flightsList.stream()
+                .filter(s->s.getFlightTo().equals(city))
+                .collect(Collectors.toList()));
+    }
+    public static void findConnectingFlights (String cityFrom, String cityTo, List<Flight> flightsList) {
+        List<Flight> flightsTo = flightsList.stream()
+                .filter(s->s.getFlightTo().equals(cityTo))
+                .collect(Collectors.toList());
+        List<Flight> flightsFrom = flightsList.stream()
+                .filter(s->s.getFlightFrom().equals(cityFrom))
+                .collect(Collectors.toList());
+        List<Flight> newList = new ArrayList<>();
+        newList.addAll(flightsTo);
+        newList.addAll(flightsFrom);
+        newList.stream().forEach(System.out::println);
+        
     }
     @Override
     public boolean equals(Object o) {
--- kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/Main.java
+++ kodilla-good-patterns/src/main/java/com/kodilla/good/patterns/Flights/Main.java
@@ -3,8 +3,9 @@ package com.kodilla.good.patterns.Flights;
 public class Main {
     public static void main(String[] args) {
 
-        Flight.findFlightsFrom("Warszawa", Flight.createFlightList());
-        Flight.findFlightsTo("Warszawa", Flight.createFlightList());
+//        Flight.findFlightsFrom("Warszawa", Flight.createFlightList());
+//        Flight.findFlightsTo("Warszawa", Flight.createFlightList());
+        Flight.findConnectingFlights("Warszawa", "Gdańsk", Flight.createFlightList());
     }
 }
 
