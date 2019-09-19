3613cd6d1c2eefc0373d5acfe80c0bfe42f6fe59
rkalau5296 <rafal.kalata.itservice@gmail.com>
2019-09-19 23:56:40 +0200

    14.2 start working and finish working

--- kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
+++ kodilla-spring/src/main/java/com/kodilla/spring/calculator/Calculator.java
@@ -1,5 +1,29 @@
 package com.kodilla.spring.calculator;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+@Component
 public class Calculator {
-    Display display;
+
+    @Autowired
+    private Display display;
+
+    public double add(double a, double b){
+        display.displayValue(a+b);
+        return a+b;
+    }
+
+    public double sub(double a, double b){
+        display.displayValue(a-b);
+        return a-b;
+    }
+    public double mul(double a, double b){
+        display.displayValue(a*b);
+        return a*b;
+    }
+    public double div(double a, double b){
+        display.displayValue(a/b);
+        return a/b;
+    }
 }
---
+++ kodilla-spring/src/test/java/com/kodilla/spring/calculator/CalculatorTestSuite.java
@@ -0,0 +1,50 @@
+package com.kodilla.spring.calculator;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CalculatorTestSuite {
+  @Test
+    public void testAddMethod(){
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.add(9,3);
+    }
+    @Test
+    public void testSubMethod(){
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.sub(9,3);
+    }
+    @Test
+    public void testDivMethod(){
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.div(9,3);
+    }
+
+    @Test
+    public void testMulMethod(){
+        //Given
+        ApplicationContext context =
+                new AnnotationConfigApplicationContext("com.kodilla.spring");
+        Calculator calculator = context.getBean(Calculator.class);
+        //When
+        calculator.mul(9,3);
+    }
+}
