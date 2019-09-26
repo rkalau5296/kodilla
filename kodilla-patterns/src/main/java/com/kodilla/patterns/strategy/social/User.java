846b0f38de1ea988a0ab70aaacac1dd887099e83
rkalata <rafal.kalata@itmagination.com>
2019-09-26 14:58:29 +0200

    15.3 start working

---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -0,0 +1,9 @@
+package com.kodilla.patterns.strategy.social;
+
+public class User {
+    final private String name;
+
+    public User(String name) {
+        this.name = name;
+    }
+}
---
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/ZGeneration.java
@@ -0,0 +1,7 @@
+package com.kodilla.patterns.strategy.social;
+
+public class ZGeneration extends User {
+    public ZGeneration(String name) {
+        super(name);
+    }
+}
