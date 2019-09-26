5acbc6a8e8e04143cad7ae4c980cf162fe37faf0
rkalata <rafal.kalata@itmagination.com>
2019-09-26 15:33:43 +0200

    15.3 keep working

--- kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
+++ kodilla-patterns/src/main/java/com/kodilla/patterns/strategy/social/User.java
@@ -2,8 +2,19 @@ package com.kodilla.patterns.strategy.social;
 
 public class User {
     final private String name;
+    protected SocialPublisher socialPublisher;
+
+    public String getName() {
+        return name;
+    }
 
     public User(String name) {
         this.name = name;
+    }
+    public void setSocialPublisher(SocialPublisher socialPublisher) {
+        this.socialPublisher = socialPublisher;
+    }
+    public String sharePost(){
+        return socialPublisher.share();
     }
 }
---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/strategy/social/UserTestSuite.java
@@ -0,0 +1,22 @@
+package com.kodilla.patterns.strategy.social;
+
+import com.kodilla.patterns.strategy.social.publishers.FacebookPublisher;
+import org.junit.Test;
+
+public class UserTestSuite {
+    @Test
+    public void testDefaultSharingStrategies() {
+        //Given
+        User peter = new Millenials("Peter Johnson");
+        User robert = new YGeneration("Robert Jackson");
+        User mike = new ZGeneration("Mike Tyson");
+
+        //When
+        String peterShouldPost = peter.sharePost();
+        System.out.println("Peter should: " + peterShouldPost);
+        String robertShouldPost = robert.sharePost();
+        System.out.println("Robert should: " + robertShouldPost);
+        String mikeShouldPost = mike.sharePost();
+        System.out.println("Mike should: " + mikeShouldPost);
+    }
+}
