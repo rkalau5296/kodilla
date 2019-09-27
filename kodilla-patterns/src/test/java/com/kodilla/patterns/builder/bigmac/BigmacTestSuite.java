273b6d3cc7d158a9a04a1084b065266eb5833e90
rkalata <rafal.kalata@itmagination.com>
2019-09-27 13:29:07 +0200

    15.5 finish work

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/builder/bigmac/BigmacTestSuite.java
@@ -0,0 +1,24 @@
+package com.kodilla.patterns.builder.bigmac;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+public class BigmacTestSuite {
+    @Test
+    public void testBigmacNew() {
+        //Given
+        Bigmac bigmac = new Bigmac.BigmacBuilder()
+                .ingredient("Salad")
+                .ingredient("Onion")
+                .ingredient("Becon")
+                .bun("With sezam")
+                .sauce("1000 island")
+                .ingredient("Chilli")
+                .build();
+        System.out.println(bigmac);
+        //When
+        int howManyIngredients = bigmac.getIngredients().size();
+        //Then
+        Assert.assertEquals(4, howManyIngredients);
+    }
+}
