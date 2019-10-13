32523f40aa6d62d1c891d31b6577079dc4c45710
rkalau5296 <rafal.kalata.itservice@gmail.com>
2019-10-14 00:11:31 +0200

    fix repo

--- kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
+++ kodilla-hibernate/src/main/java/com/kodilla/hibernate/tasklist/dao/TaskListDao.java
@@ -5,6 +5,6 @@ import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface TaskListDao extends CrudRepository<TaskList, String> {
-    List<TaskList> findByListName(String listName);
+public interface TaskListDao extends CrudRepository<TaskList, Integer> {
+    List<TaskList> findByListName(String listName);
 }
