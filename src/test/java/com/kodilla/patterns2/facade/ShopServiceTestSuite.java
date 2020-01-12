25a7fa0ecb66a00d69e42be2d811e5a25e0af46a
rkalau <rafal.kalata.itservice@gmail.com>
2020-01-12 22:38:30 +0100

    facade cd

--- src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
+++ src/test/java/com/kodilla/patterns2/facade/ShopServiceTestSuite.java
@@ -1,6 +1,10 @@
 package com.kodilla.patterns2.facade;
 
 import com.kodilla.patterns2.facade.ShopService;
+import com.kodilla.patterns2.facade.api.ItemDto;
+import com.kodilla.patterns2.facade.api.OrderDto;
+import com.kodilla.patterns2.facade.api.OrderFacade;
+import com.kodilla.patterns2.facade.api.OrderProcessingException;
 import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -15,7 +19,8 @@ public class ShopServiceTestSuite {
 
     @Autowired
     private ShopService shopService;
-
+    @Autowired
+    private OrderFacade orderFacade;
     @Test
     public void testShopServiceSubmitOrder() {
         long orderId = shopService.openOrder(1L);
@@ -57,6 +62,19 @@ public class ShopServiceTestSuite {
             }
         } else {
             System.out.println("Access denied. User is not authenticated.");
+        }
+    }
+    @Test
+    public void testShopFacade(){
+        OrderDto order = new OrderDto();
+        order.addItem(new ItemDto(10L, 2));
+        order.addItem(new ItemDto(215L, 1));
+        order.addItem(new ItemDto(25L, 1));
+        order.addItem(new ItemDto(11L, 3));
+        try {
+            orderFacade.processOrder(order, 1L);
+        } catch (OrderProcessingException e) {
+
         }
     }
 }
