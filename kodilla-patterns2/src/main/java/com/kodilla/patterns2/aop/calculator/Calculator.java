81fa434bbf98bb5f88859daa6646cb94df910521
rkalata <rkalata@kabbage.com>
2020-01-16 15:08:47 +0100

    24.3 start

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Calculator.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/aop/calculator/Calculator.java
@@ -2,6 +2,8 @@ package com.kodilla.patterns2.aop.calculator;
 
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+
 @Component
 public class Calculator {
     public double add(double x, double y) {
@@ -21,5 +23,15 @@ public class Calculator {
             throw new ArithmeticException("Divide by zero!");
         }
         return x / y;
+    }
+
+    public BigDecimal factorial(BigDecimal n) {
+        if(n.compareTo(BigDecimal.ONE)>0){
+            return n.multiply(factorial(n.subtract(BigDecimal.ONE)));
+        } else if (n.equals(BigDecimal.ONE) || n.equals(BigDecimal.ZERO)) {
+            return BigDecimal.ONE;
+        } else{
+            throw new ArithmeticException("Factorial argument is negative");
+        }
     }
 }
