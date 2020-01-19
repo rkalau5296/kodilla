fdd94e24e6d86edce827d1191ba4723847674718
rkalau <rafal.kalata.itservice@gmail.com>
2020-01-19 16:15:33 +0100

    24.5 start working

---
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -0,0 +1,30 @@
+package com.kodilla.patterns2.adapter.bookclassifier;
+
+import com.kodilla.patterns2.adapter.bookclasifier.MedianAdapter;
+import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
+import org.junit.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.Assert.assertEquals;
+
+public class MedianAdapterTestSuite {
+    @Test
+    public void publicationYearMedianTest(){
+        //Given
+        Set<Book> books = new HashSet<>();
+        books.add(new Book("Sienkiewicz", "Rodzina Polanieckich", 1978, "Yes"));
+        books.add(new Book("Sienkiewicz", "Krzyzacy", 1980, "Yes"));
+        books.add(new Book("Sienkiewicz", "Potop", 1985, "Yes"));
+        books.add(new Book("Sienkiewicz", "Pan Wlodyjowski", 1990, "Yes"));
+        books.add(new Book("Sienkiewicz", "Ogniem i mieczem", 1975, "Yes"));
+
+        MedianAdapter medianAdapter = new MedianAdapter();
+        //When
+        int publicationYearMedian = medianAdapter.publicationYearMedian(books);
+        //Then
+        System.out.println(publicationYearMedian);
+        assertEquals(0, publicationYearMedian,0);
+    }
+}
