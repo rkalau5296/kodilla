17d2ffa5440ee0fc8b7d881d64b99295c68286a8
rkalata <rkalata@kabbage.com>
2020-01-29 16:08:35 +0100

    fix task 24.5

--- kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
+++ kodilla-patterns2/src/main/java/com/kodilla/patterns2/adapter/bookclasifier/MedianAdapter.java
@@ -1,17 +1,28 @@
 package com.kodilla.patterns2.adapter.bookclasifier;
 
-import com.kodilla.patterns2.adapter.bookclasifier.librarya.Book;
 import com.kodilla.patterns2.adapter.bookclasifier.librarya.Classifier;
+import com.kodilla.patterns2.adapter.bookclasifier.libraryb.BookSignature;
 
+
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Set;
 
 public class MedianAdapter extends MedianAdaptee implements Classifier {
-    @Override
-    public int publicationYearMedian(Set<Book> bookset) {
-        //TODO publicationYearMedian method
-        //media adaptee
 
-        //przeiterować set skonwertować z setu na mapę, i użyć z medianadaptee  medianPublicationYear i ona policzyc tę medianę
-        return 1980;
+    @Override
+    public int publicationYearMedian(Set<com.kodilla.patterns2.adapter.bookclasifier.librarya.Book> bookset) {
+        if(bookset.size() == 0) return 0;
+
+        Map<BookSignature, com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book> bookMap = new HashMap<>();
+        for(com.kodilla.patterns2.adapter.bookclasifier.librarya.Book book: bookset){
+
+            com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book bookConverted =
+                    new com.kodilla.patterns2.adapter.bookclasifier.libraryb.Book(book.getAuthor(), book.getTitle(), book.getPublicationYear());
+
+            bookMap.put(new BookSignature("signature"), bookConverted);
+        }
+
+        return medianPublicationYear(bookMap);
     }
 }
--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -6,6 +6,7 @@ import org.junit.Test;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertEquals;
 
@@ -19,7 +20,9 @@ public class MedianAdapterTestSuite {
         books.add(new Book("Sienkiewicz", "Potop", 1985, "Yes"));
         books.add(new Book("Sienkiewicz", "Pan Wlodyjowski", 1990, "Yes"));
         books.add(new Book("Sienkiewicz", "Ogniem i mieczem", 1975, "Yes"));
-
+//        books.stream()
+//                .map(Book::getPublicationYear)
+//                .collect(Collectors.toList());
         MedianAdapter medianAdapter = new MedianAdapter();
         //When
         int publicationYearMedian = medianAdapter.publicationYearMedian(books);
